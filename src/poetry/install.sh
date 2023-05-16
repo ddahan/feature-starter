3cfd416958eb094bf09295bb59806a8327f25b6e
ddahan <david.dahan3@gmail.com>
2023-05-16 11:15:04 +0000

    test

--- src/poetry/install.sh
+++ src/poetry/install.sh
@@ -4,6 +4,5 @@ set -e
 echo "Installation feature Poetry"
 echo "The provided version is: ${VERSION}"
 
-export POETRY_VIRTUALENVS_CREATE=false
 # (https://stackoverflow.com/a/54763270/2255491)
-pip install "poetry==${VERSION}"
+POETRY_VIRTUALENVS_CREATE=false pip install "poetry==${VERSION}"
